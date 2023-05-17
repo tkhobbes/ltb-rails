dc1e4d8be7a8c4ac4ecf7f1fa85dd4bac7278965
tkhobbes <9db7-ex0s@spamex.com>
2023-05-17 11:12:52 +0200

    tests for book show and index page pass

--- spec/requests/books_spec.rb
+++ spec/requests/books_spec.rb
@@ -1,10 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe 'Books' do
-  describe 'GET /index' do
-    it 'returns http success' do
-      get books_path
-      expect(response).to have_http_status(:success)
+  describe 'authentication' do
+    context 'does not need to log in' do
+      it 'can access the book index path' do
+        get books_path
+        expect(response).to have_http_status(:success)
+      end
+
+      it 'can access the book show path' do
+        book = create(:book)
+        get book_path(book)
+        expect(response).to have_http_status(:success)
+      end
     end
   end
 end

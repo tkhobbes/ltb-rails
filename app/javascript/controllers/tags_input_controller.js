512faf896ba0c244ef40e5c3774c291ac2d30309
tkhobbes <9db7-ex0s@spamex.com>
2023-06-01 10:32:03 +0200

    added i18n npm package and simple translation functionality for tag input

--- app/javascript/controllers/tags_input_controller.js
+++ app/javascript/controllers/tags_input_controller.js
@@ -1,13 +1,20 @@
 import { Controller } from "@hotwired/stimulus";
 import TomSelect from "tom-select";
+import { I18n } from "i18n-js";
+import { translations } from "../../../config/locales/locales.json";
 
 // Connects to data-controller="tags-input"
 export default class extends Controller {
+  static values = {
+    locale: String,
+  };
   connect() {
+    const i18n = new I18n(translations);
+    i18n.locale = this.localeValue;
     new TomSelect(this.element, {
       plugins: {
         remove_button: {
-          title: "Remove Item",
+          title: i18n.t("tags-input.remove-tag"),
         },
       },
     });

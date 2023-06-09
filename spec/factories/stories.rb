610f11f8b05f1dac83cbfbab6a5b27c6e0330715
tkhobbes <9db7-ex0s@spamex.com>
2023-06-09 14:04:35 +0200

    system spec for tags

--- spec/factories/stories.rb
+++ spec/factories/stories.rb
@@ -1,5 +1,6 @@
 FactoryBot.define do
   factory :story do
+    title { 'Paperino e il mistero del vecchio castello' }
     code { 'I TL  303-AP' }
     url { 'https://inducks.org/story.php?c=I+TL++303-AP' }
     published { 1961 }

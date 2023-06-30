d25a51696f13ffb135e6f8ed9a55c928e7c341f3
tkhobbes <9db7-ex0s@spamex.com>
2023-06-30 10:23:50 +0200

    changed flashes to use bulma-toast

--- app/javascript/controllers/close_button_controller.js
+++ app/javascript/controllers/close_button_controller.js
@@ -1,13 +1,19 @@
 import { Controller } from "@hotwired/stimulus";
+import { toast } from "bulma-toast";
 
 // Connects to data-controller="close-button"
 export default class extends Controller {
+  static values = { message: String, type: String };
   connect() {
-    setTimeout(() => {
-      this.element.remove();
-    }, 3000);
-  }
-  click() {
-    this.element.remove();
+    toast({
+      message: this.messageValue,
+      type: this.typeValue,
+      duration: 10000,
+      dismissible: true,
+      position: "top-center",
+      pauseOnHover: true,
+      closeOnClick: true,
+      opacity: 0.9,
+    });
   }
 }

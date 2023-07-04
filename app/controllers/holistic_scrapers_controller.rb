d9a266195b71b726622e75467ff1626b69d13374
tkhobbes <9db7-ex0s@spamex.com>
2023-07-04 12:01:41 +0200

    turbo progress bar styled

--- app/controllers/holistic_scrapers_controller.rb
+++ app/controllers/holistic_scrapers_controller.rb
@@ -6,7 +6,7 @@ class HolisticScrapersController < ApplicationController
     respond_to do |format|
       format.html do
         if book.id.present?
-          redirect_to books_path(book), notice: t('.all-added')
+          redirect_to book_path(book), notice: t('.all-added')
         else
           redirect_to new_books_search_path, notice: t('.not-added')
         end

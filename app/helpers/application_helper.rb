8214069e4558b5cfc694cead8af1a573a3e04e75
tkhobbes <9db7-ex0s@spamex.com>
2023-09-14 14:17:30 +0200

    added turbo-flash (not working)

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,11 @@
 # Standard Rails application helper
 module ApplicationHelper
   include Pagy::Frontend
+
+  # render flash messages - needed for hot_flash
+  def render_flash
+    return if @_flash_rendered
+
+    render partial: 'shared/flashes'
+  end
 end

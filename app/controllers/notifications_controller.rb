3cc23837c4087b0af423810fdd50103bb7b4b27b
tkhobbes <9db7-ex0s@spamex.com>
2023-09-19 10:35:27 +0200

    notifications are read when clicked

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -3,4 +3,20 @@ class NotificationsController < ApplicationController
   def index
     @notifications = Notification.where(recipient: current_user).order(created_at: :desc).includes([:recipient])
   end
+
+  def read
+    @notification = Notification.find(params[:id])
+    @notification.update(read_at: Time.zone.now)
+    respond_to do |format|
+      format.turbo_stream do
+        render turbo_stream: turbo_stream.replace(@notification, partial: 'notifications/notification',
+                                                                 locals: { notification: @notification })
+      end
+    end
+  end
+
+  def unread
+    @notification = Notification.find(params[:id])
+    @notification.update(read_at: nil)
+  end
 end

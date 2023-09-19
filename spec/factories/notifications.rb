89f98dedc87f6c0434f579152aa33ebc6e508a43
tkhobbes <9db7-ex0s@spamex.com>
2023-09-19 16:56:24 +0200

    specs / tests fixed

--- spec/factories/notifications.rb
+++ spec/factories/notifications.rb
@@ -1,8 +1,8 @@
 FactoryBot.define do
   factory :notification do
-    recipient { nil }
-    type { "" }
-    params { "" }
-    read_at { "2023-09-14 14:29:29" }
+    recipient factory: %i[user]
+    type { 'book' }
+    params factory: %i[book]
+    read_at { '2023-09-14 14:29:29' }
   end
 end

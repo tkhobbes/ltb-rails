fbe3a440af7549645574a1a4ccd527c7b54ea6a6
tkhobbes <9db7-ex0s@spamex.com>
2025-04-23 14:46:15 +0200

    artist with name ? no longer scraped

--- app/services/scraper/role_scraper.rb
+++ app/services/scraper/role_scraper.rb
@@ -14,7 +14,10 @@ module Scraper
     def scrape
       roles = {}
       Role.tasks.each_key do |task|
-        roles[task] = artist_code(one_role(task)) if one_role(task).present?
+        code = artist_code(one_role(task))
+        next if code == '%3F' # do not include artists with code "Question Mark"
+
+        roles[task] = code if one_role(task).present?
       end
       return ReturnRoles.new(message: I18n.t('services.scraper.no-scraper')) unless @scraper.created?
       return ReturnRoles.new(message: I18n.t('services.scraper.no-roles')) if roles.blank?
@@ -47,7 +50,7 @@ module Scraper
 
     # return the artist code from a string like https://inducks.org/creator.php?c=RSc
     def artist_code(url)
-      url.sub(/^.*c=/, '')
+      url.sub(/^.*c=/, '') if url.present?
     end
   end
 end
